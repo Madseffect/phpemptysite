be6a54efc5618dff350b203ff0a1baf3a236b068
Madseffect <themadsk@gmail.com>
2016-11-30 11:11:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
+$uri = "http://restmads.cloudapp.net/FridgeService.svc/billed/";
 
+
+$output = file_get_contents($uri);
+
+$convertToAssociativeArray = true;
+$urioutput = json_decode($output, $convertToAssociativeArray);
+
+
+
+foreach ($urioutput as &$value)
+{
+$billedeOutput = file_get_contents($value);
+echo '<img src="data:image/jpeg;base64,' . $billedeOutput . '" />';
 ?>
